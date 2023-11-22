8080bcb7b239502c578f0bf2fdd4c9a5107ea131
uwebury <142432719+uwebury@users.noreply.github.com>
2023-11-22 11:52:39 +0100

    Added create.element and clearButton function

--- js/index.js
+++ js/index.js
@@ -3,11 +3,18 @@ console.clear();
 const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
+const toastContainerMessage = document.querySelector(
+  '[data-js="toast-container__message"]'
+);
 
 addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+  const toastMessage = document.createElement("li");
+  toastMessage.textContent = toastContainerMessage.innerHTML;
+  toastMessage.classList.add("toast-container__message");
+  toastContainer.append(toastMessage);
 });
 
 clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+  const toastContainer = document.querySelector('[data-js="toast-container"]');
+  toastContainer.innerHTML = "";
 });
